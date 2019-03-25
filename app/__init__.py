e9606b7bb2dfc3909d115082c38d851f5e0c664c
z0glen <zgbauer@gmail.com>
2019-03-25 19:25:41 -0400

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+#from flask_sqlalchemy import SQLAlchemy
+#from flask_migrate import Migrate
+
+app = Flask(__name__)
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
+
+from app import routes
