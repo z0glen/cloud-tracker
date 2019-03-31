296f8c3c23b5149ce4d6be36ce93e55aa6b392eb
z0glen <zgbauer@gmail.com>
2019-03-31 02:15:27 -0400

    removing secrets

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,5 @@
 from flask import Flask
-#from flask_sqlalchemy import SQLAlchemy
-#from flask_migrate import Migrate
 
 app = Flask(__name__)
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -5,16 +5,21 @@ import datetime
 import smtplib
 import atexit
 import json
+import os
 
 from email.message import EmailMessage
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from app import app
 
+DARK_SKY_KEY = os.environ.get('DARK_SKY_KEY')
+ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL')
+ADMIN_EMAIL_PASSWORD = os.environ.get('ADMIN_EMAIL_PASSWORD')
+
+email_list = ['yakbakzak@gmail.com', 'mathmania3@gmail.com']
+
 def daily_forecast(t):
-        # t = int(time.time())
-        r = requests.get('https://api.darksky.net/forecast/5552df4c94df0d62968b69b91fc61212/42.3601,-71.0589,' + str(t) + '?exclude=currently,flags')
-        # print(r.json())
+        r = requests.get('https://api.darksky.net/forecast/' + DARK_SKY_KEY + '/42.3601,-71.0589,' + str(t) + '?exclude=currently,flags')
         sunrise = r.json()['daily']['data'][0]['sunriseTime']
         sunset = r.json()['daily']['data'][0]['sunsetTime']
         for i, hour in enumerate(r.json()['hourly']['data']):
@@ -32,7 +37,6 @@ def daily_forecast(t):
 
 def five_day_forecast():
     data = {}
-    # strings = []
     string = ""
     for i in range(1, 6):
         t = int(time.time() + i*24*60*60)
@@ -40,28 +44,21 @@ def five_day_forecast():
         data[d] = daily_forecast(t)
         sunrise_string = datetime.datetime.fromtimestamp(data[d]['sunrise']).strftime('%H:%M')
         sunset_string = datetime.datetime.fromtimestamp(data[d]['sunset']).strftime('%H:%M')
-        # strings.append("On " + d + ", sunrise will be " + data[d]['sunrise_data'] + " at " + sunrise_string + " and sunset will be " + data[d]["sunset_data"] + " at " + sunset_string + "\n")
         string += "On " + d + ", sunrise will be " + data[d]['sunrise_data'] + " at " + sunrise_string + " and sunset will be " + data[d]["sunset_data"] + " at " + sunset_string + "\n"
     return string
 
 def send_email():
-    # message = "Test message"
-    # msg = MIMEMultipart()
-    print('Composing email...')
-    msg = EmailMessage()
-    msg.set_content(five_day_forecast())
-    msg['Subject'] = '[Cloud Tracker] Five Day Forecast'
-    msg['From'] = "yakbakzak@gmail.com"
-    msg['To'] = "yakbakzak@gmail.com"
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.starttls()
-    s.login("yakbakzak@gmail.com", "thpkzspxhiqcrmbe")
-    # message = "Test email"
-    # s.sendmail("yakbakzak@gmail.com", "yakbakzak@gmail.com", message)
-    s.send_message(msg)
+    s.login(ADMIN_EMAIL, ADMIN_EMAIL_PASSWORD)
+    for to_address in email_list:
+        msg = EmailMessage()
+        msg.set_content(five_day_forecast())
+        msg['Subject'] = '[Cloud Tracker] Five Day Forecast'
+        msg['From'] = ADMIN_EMAIL
+        msg['To'] = to_address
+        s.send_message(msg)
     s.quit()
-    print('Email sent!')
-    # password = thpkzspxhiqcrmbe
 
 scheduler = BackgroundScheduler(daemon=True)
 scheduler.add_job(func=send_email, trigger='interval', hours=24)
@@ -69,17 +66,14 @@ scheduler.start()
 
 atexit.register(lambda: scheduler.shutdown())
 
-# print(five_day_forecast())
-
 @app.route("/")
 def index():
-    # send_email()
     return render_template('index.html')
 
 @app.route("/data")
 def data():
     t = int(time.time())
-    r = requests.get('https://api.darksky.net/forecast/5552df4c94df0d62968b69b91fc61212/42.3601,-71.0589,' + str(t) + '?exclude=currently,flags')
+    r = requests.get('https://api.darksky.net/forecast/' + DARK_SKY_KEY + '/42.3601,-71.0589,' + str(t) + '?exclude=currently,flags')
     return jsonify(r.json())
 
 @app.route("/form")
@@ -88,7 +82,7 @@ def form():
     t = int(datetime.datetime.strptime(d, "%Y-%m-%d").timestamp())
     lat = request.args.get('lat')
     long = request.args.get('long')
-    r = requests.get('https://api.darksky.net/forecast/5552df4c94df0d62968b69b91fc61212/' + lat + ',' + long + ',' + str(t) + '?exclude=currently,flags')
+    r = requests.get('https://api.darksky.net/forecast/' + DARK_SKY_KEY + '/' + lat + ',' + long + ',' + str(t) + '?exclude=currently,flags')
     return jsonify(r.json())
 
 if __name__ == '__main__':
