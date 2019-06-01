a4ebb54eeefdb6425ed6a4fa7e1bbe171e7c5f49
z0glen <zgbauer@gmail.com>
2019-06-01 13:23:39 -0400

    fix(web, models): update default daily_email field to True

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    daily_email = db.Column(db.Boolean, default=False, nullable=False)
+    daily_email = db.Column(db.Boolean, default=True, nullable=False)
 
     def __repr__(self):
         return '<User {}>'.format(self.email)
